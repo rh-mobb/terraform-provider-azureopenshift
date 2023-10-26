370e56a56735e80a9d09be2c064e02eb10eb2ff8
Dustin Scott <dscott@redhat.com>
2023-10-26 13:55:59 -0500

    refactor: remove conditional statements and move creds var
    Signed-off-by: Dustin Scott <dscott@redhat.com>

--- azureopenshift/auth/auth.go
+++ azureopenshift/auth/auth.go
@@ -35,7 +35,6 @@ type DefaultAroCredential struct {
 }
 
 func NewDefaultAroCredential(config Config) (*DefaultAroCredential, error) {
-	var creds []azcore.TokenCredential
 	var errorMessages []string
 
 	options := &azidentity.ClientSecretCredentialOptions{
@@ -43,18 +42,16 @@ func NewDefaultAroCredential(config Config) (*DefaultAroCredential, error) {
 	}
 
 	clientSecretCred, err := azidentity.NewClientSecretCredential(config.TenantId, config.ClientId, config.ClientSecret, options)
-	if err == nil {
-		creds = append(creds, clientSecretCred)
-	} else {
+	if err != nil {
 		errorMessages = append(errorMessages, "AroClientSecretCredential: "+err.Error())
 	}
 
 	cliCred, err := azidentity.NewAzureCLICredential(nil)
-	if err == nil {
-		creds = append(creds, cliCred)
-	} else {
+	if err != nil {
 		errorMessages = append(errorMessages, "AroCLICredential: "+err.Error())
 	}
+
+	creds := []azcore.TokenCredential{clientSecretCred, cliCred}
 
 	err = defaultAroCredentialConstructorErrorHandler(len(creds), errorMessages)
 	if err != nil {
